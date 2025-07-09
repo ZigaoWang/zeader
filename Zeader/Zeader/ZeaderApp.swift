25a8f6274b3f0cb482d5f4adf817dcce79411472
Zigao Wang <zigao.wang24@gmail.com>
2025-07-09 23:56:46 +0800

    feat: empty multiplatform xcode project

---
+++ Zeader/Zeader/ZeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeaderApp.swift
+//  Zeader
+//
+//  Created by Zigao Wang on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ZeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
